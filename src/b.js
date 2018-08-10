18a8797b46a22c3bfa0255168670b73454348c72
201311185강성규 <tj5192@naver.com>
2018-08-10 12:44:54 +0900

    Update b.js

--- src/b.js
+++ src/b.js
@@ -41,9 +41,9 @@ var backImage = new Image(); // 배경화면
  };
 ////////////////////////////////////////////////////////////////////////
 ganvas.width = window.screen.width-10;
-ganvas.height = (window.screen.width-10)*(16/9);
 canvas.width = (window.screen.width-10)*(0.9);
 canvas.height = (window.screen.width-10)*(11/7)*(0.9);
+
 ////////////////////////////////////////////////////////////////////////
 var tout; // setTimeout(detectCollision) 담는 변수
 var max; // 실행해서 나왔던 게임점수 중 가장 고득점 뽑는 변수
